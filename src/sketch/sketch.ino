a5389775f2dfef4dad8580324511b73a2d5243be
Alan Alencar da Silva <benparvar@users.noreply.github.com>
2025-01-23 19:07:01 -0300

    date time implementation

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -6,35 +6,47 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 16, 2);
+LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 18, 4);
 
 #define NTP_SERVER     "pool.ntp.org"
-#define UTC_OFFSET     0
+#define UTC_OFFSET     -3 * 3600
 #define UTC_OFFSET_DST 0
 
 void spinner() {
   static int8_t counter = 0;
   const char* glyphs = "\xa1\xa5\xdb";
-  LCD.setCursor(15, 1);
+  LCD.setCursor(18, 0);
   LCD.print(glyphs[counter++]);
   if (counter == strlen(glyphs)) {
     counter = 0;
   }
 }
 
+void printOnline() {
+  LCD.setCursor(0, 3);
+  LCD.print("Online");
+}
+
+void printOffLine() {
+  LCD.setCursor(0, 3);
+  LCD.print("Offline");
+}
+
 void printLocalTime() {
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
-    LCD.setCursor(0, 1);
-    LCD.println("Connection Err");
+    printOffLine();
     return;
   }
 
-  LCD.setCursor(8, 0);
-  LCD.println(&timeinfo, "%H:%M:%S");
+  LCD.setCursor(0, 0);
+  LCD.print(&timeinfo, "%d/%m/%Y  %H:%M:%S");
+  //LCD.print(&timeinfo, "%d/%m/%Y       %Z");
 
-  LCD.setCursor(0, 1);
-  LCD.println(&timeinfo, "%d/%m/%Y   %Z");
+  //LCD.setCursor(0, 1);
+  //LCD.print(&timeinfo, "%H:%M:%S            ");
+
+  printOnline();
 }
 
 void setup() {
@@ -43,11 +55,9 @@ void setup() {
   LCD.init();
   LCD.backlight();
   LCD.setCursor(0, 0);
-  LCD.print("Connecting to ");
-  LCD.setCursor(0, 1);
-  LCD.print("WiFi ");
+  LCD.print("Connecting to WiFi");
 
-  WiFi.begin("Wokwi-GUEST", "", 6);
+  WiFi.begin("Go_To_Hell", "4a5l8a6n1", 6);
   while (WiFi.status() != WL_CONNECTED) {
     delay(250);
     spinner();
@@ -59,10 +69,10 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   LCD.clear();
+ 
+  printOnline();
   LCD.setCursor(0, 0);
-  LCD.println("Online");
-  LCD.setCursor(0, 1);
-  LCD.println("Updating time...");
+  LCD.print("Updating time...  ");
 
   configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
 }
