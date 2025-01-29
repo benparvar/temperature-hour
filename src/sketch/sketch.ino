f2d6aeeed0cebecc25882783c9c834d5870a7caa
Alan Alencar da Silva <benparvar@users.noreply.github.com>
2025-01-29 20:09:15 -0300

    code adjusting

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,7 +1,3 @@
-// Learn about the ESP32 WiFi simulation in
-// https://docs.wokwi.com/guides/esp32-wifi
-// https://wokwi.com/projects/321525495180034642
-
 #include <WiFi.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
@@ -11,6 +7,9 @@ LiquidCrystal_I2C LCD = LiquidCrystal_I2C(0x27, 18, 4);
 #define NTP_SERVER     "pool.ntp.org"
 #define UTC_OFFSET     -3 * 3600 // GMT -3
 #define UTC_OFFSET_DST 0
+
+#define WIFI_USERNAME "IoT"
+#define WIFI_PASSWORD "NcC-1701"
 
 void spinner() {
   static int8_t counter = 0;
@@ -22,42 +21,13 @@ void spinner() {
   }
 }
 
-void printOnline() {
-  LCD.setCursor(0, 3);
-  LCD.print("Online");
-}
+void connectToWifi() {
+  Serial.println("connectToWifi");
 
-void printOffLine() {
-  LCD.setCursor(0, 3);
-  LCD.print("Offline");
-}
-
-void printLocalTime() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    printOffLine();
-    return;
-  }
-
-  LCD.setCursor(0, 0);
-  LCD.print(&timeinfo, "%d/%m/%Y  %H:%M:%S");
-  //LCD.print(&timeinfo, "%d/%m/%Y       %Z");
-
-  //LCD.setCursor(0, 1);
-  //LCD.print(&timeinfo, "%H:%M:%S            ");
-
-  printOnline();
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  LCD.init();
-  LCD.backlight();
   LCD.setCursor(0, 0);
   LCD.print("Connecting to WiFi");
 
-  WiFi.begin("user", "pwd", 6);
+  WiFi.begin(WIFI_USERNAME, WIFI_PASSWORD, 6);
   while (WiFi.status() != WL_CONNECTED) {
     delay(250);
     spinner();
@@ -69,15 +39,59 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   LCD.clear();
- 
   printOnline();
   LCD.setCursor(0, 0);
   LCD.print("Updating time...  ");
+}
+
+void printOnline() {
+  Serial.println("printOnline");
+
+  LCD.setCursor(0, 3);
+  LCD.print("Online");
+}
+
+void printOffLine() {
+  Serial.println("printOffLine");
+
+  LCD.setCursor(0, 3);
+  LCD.print("Offline");
+}
+
+void printLocalTime() {
+  Serial.println("printLocalTime");
+
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    printOffLine();
+    return;
+  }
+
+  LCD.setCursor(0, 0);
+  LCD.print(&timeinfo, "%d/%m/%Y  %H:%M:%S");
+
+  printOnline();
+}
+
+void printTemperature() {
+ Serial.println("printTemperature");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("setup");
+
+  LCD.init();
+  LCD.backlight();
+
+  connectToWifi();
 
   configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
 }
 
 void loop() {
   printLocalTime();
+  printTemperature();
   delay(250);
 }
